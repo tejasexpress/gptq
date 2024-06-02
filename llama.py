ef61e3982796631f09a4224ab8d9264ddefb1b07
tejasexpress <45305155+tejasexpress@users.noreply.github.com>
2024-06-02 21:21:41 +0530

    Update llama.py

--- llama.py
+++ llama.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
         help='Whether to run the RTN baseline.'
     ) 
     parser.add_argument(
-        '--wbits', type=int, default=16, choices=[2, 3, 4, 8, 16],
+        '--wbits', type=int, default=16, choices=[2, 3, 4, 6, 8, 16],
         help='#bits to use for quantization; use 16 for evaluating base model.'
     )
     parser.add_argument(
